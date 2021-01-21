db464da9680dc621613bcdd4ff8c25835b16be99
Heikki Ahonen <heikki.ahonen@centria.fi>
2021-01-21 08:33:29 +0200

    Init

---
+++ example_variables.js
@@ -0,0 +1,18 @@
+const t = [1, 2, 3]
+const tSquared = t.map(p => p * p)
+console.log(tSquared)
+// tSquared is now [1, 4, 9]
+
+function product(a, b) {
+  return a * b
+}
+
+const product2 = (a, b) => a * b
+
+const product3 = (a, b) => {
+  return (a * b)
+}
+
+const product4 = function(a, b) {
+  return (a * b)
+}
