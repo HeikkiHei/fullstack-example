01c677fb22445eca3a3d686e9a8f4413f43f1eb2
Heikki Ahonen <heikki.ahonen@centria.fi>
2021-01-25 09:00:50 +0200

    Structure

---
+++ part1-example/src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+// This is the right place to define a component
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
